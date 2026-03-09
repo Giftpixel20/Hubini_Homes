e500c81a742ee867de20bd701fccfdfa3df66eda
Giftpixel20 <giftpixel20@gmail.com>
2026-03-09 18:43:54 +0100

    Update database.js

--- config/database.js
+++ config/database.js
@@ -8,7 +8,7 @@ const createPool = () => {
       host: process.env.DB_HOST || '127.0.0.1',
       port: parseInt(process.env.DB_PORT) || 3306,
       user: process.env.DB_USER || 'root',
-      password: process.env.DB_PASS || '',
+      password: process.env.DB_PASSWORD || '',
       database: process.env.DB_NAME || 'hubini_db',
       waitForConnections: true,
       connectionLimit: 10,
